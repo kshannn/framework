d45489e29f56f2c46307352f231c2171a461293f
kshannn <k.leshan@hotmail.com>
2021-07-09 06:32:07 +0000

    Completed lab 16

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { User } = require('../models');
 
 // import caolan forms
-const { createRegistrationForm, bootstrapField } = require('../forms');
+const { createRegistrationForm, createLoginForm, bootstrapField } = require('../forms');
 
 
 // display registration page
@@ -42,9 +42,74 @@ router.post('/register', async (req,res)=>{
 
 // user log in 
 router.get('/login', (req,res) => {
-    res.render('users/login')
+    const loginForm = createLoginForm();
+    res.render('users/login',{
+        'forms': loginForm.toHTML(bootstrapField)
+    })
 })
 
+// process user log in information
+router.post('/login', (req,res) => {
+    const loginForm = createLoginForm();
+    loginForm.handle(req,{
+        'success': async (form) => {
+            // process login
+            let user = await User.where({
+                'email': form.data.email
+            }).fetch({
+                require: false
+            })
+
+            if(!user) {
+                req.flash("error_messages","Sorry, the authentication details you provided are invalid")
+                res.redirect('/users/login')
+            } else {
+                // check if pw match
+                if(user.get('password') === form.data.password){
+                    // add to session the user details if login succeed
+                    req.session.user = {
+                        id: user.get('id'),
+                        username: user.get('username'),
+                        email: user.get('email')
+                    }
+                    req.flash('success_messages','Welcome back,' + user.get('username'))
+                    res.redirect('/users/profile')
+                } else {
+                    req.flash('error_messages','Sorry, the authentication details you provided are invalid')
+                    res.redirect('/users/login')
+                }
+                
+            }
+        },
+        'error': (form) => {
+            req.flash('error_messages', 'There are some problems logging you in. Please fill the form again.')
+            res.render('users/login',{
+                'forms': form.toHTML(bootstrapField)
+            })
+        }
+    })
+})
+
+// display profile 
+router.get('/profile', (req,res) => {
+    const user = req.session.user
+
+    if(!user){
+        req.flash('error_messages','You do not have the permission to view this')
+        res.redirect('/users/login')
+    } else {
+        res.render('users/profile', {
+            'user': user
+        })
+    }
+})
+
+router.get('/logout', (req,res) => {
+    // remove the session
+    req.session.user = null;
+    req.flash('success_messages','Goodbye')
+    res.redirect('/users/login')
+})
 
 
 module.exports = router;
